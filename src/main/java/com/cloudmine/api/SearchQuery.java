033f43e3e86a716813d7f260d013a92ae2da1332
JohnMcCarthy <john@cloudmine.me>
2013-07-12 15:40:46 -0400

    Comment class

--- src/main/java/com/cloudmine/api/SearchQuery.java
+++ src/main/java/com/cloudmine/api/SearchQuery.java
@@ -28,9 +28,9 @@ public class SearchQuery {
     }
 
     /**
-     * Construct a {@link CombinableFilterValue} that will search
-     * @param cls
-     * @return
+     * Construct a {@link CombinableFilterValue} that will search for objects of the given class
+     * @param cls the class to search for
+     * @return a {@link CombinableFilterValue} that can either be turned into a search query or further specified
      */
     public static CombinableFilterValue filter(Class cls) {
         SearchQuery searchQuery = new SearchQuery();
@@ -38,20 +38,39 @@ public class SearchQuery {
         return searchQuery.PropertyName(JsonUtilities.CLASS_KEY).equal(ClassNameRegistry.forClass(cls));
     }
 
+    /**
+     * Construct a {@link SubObject} that lets you create a search on a subobject property
+     * @param name the key for the subobject
+     * @return
+     */
     public static SubObject subObject(String name) {
         return new SearchQuery().SubObject(name);
     }
 
+    /**
+     * A subobject is a non string/integer object that is a child of a CMObject. You can either drill down further, to a
+     * subobject on the SubObject, or filter on a property on the SubObject
+     */
     public class SubObject {
         private SubObject(String name) {
             queryBuilder.append(name);
         }
 
+        /**
+         * Drill down further
+         * @param name the variable name of the subobject to drill down to
+         * @return another SubObject
+         */
         public SubObject subObject(String name) {
             subObjectSeparator();
             return new SubObject(name);
         }
 
+        /**
+         * Filter on the current subobject
+         * @param name the property to filter on
+         * @return a PropertyName
+         */
         public PropertyName filter(String name) {
             openFilter();
             return PropertyName(name);
@@ -62,14 +81,30 @@ public class SearchQuery {
         queryBuilder.append(".");
     }
 
+    /**
+     * Represents the state of a variable name being selected for searching on. Provides various methods for filtering on 
+     * that variable name
+     */
     public class PropertyName {
         //Geopoint filters
+
+        /**
+         * This variable name contains a CMGeoPoint and we want to find all the objects near the specified point 
+         * @param geoPoint objects will be returned in order based on how close they are to this point
+         * @return a CMGeoPointFilterValue that allows for setting a distance limit
+         */
         public CMGeoPointFilterValue near(CMGeoPointInterface geoPoint) {
             queryBuilder.append(" near (").append(geoPoint.getLongitude()).append(", ").append(geoPoint.getLatitude()).append(")");
             return new CMGeoPointFilterValue();
         }
 
         //String filter values
+
+        /**
+         * Find objects with values not equal to the given value
+         * @param value the value to compare to this variable name
+         * @return a {@link CombinableFilterValue} that lets you add additional filters
+         */
         public CombinableFilterValue notEqual(String value) {
             notEqual();
             quote(value);
@@ -80,6 +115,11 @@ public class SearchQuery {
             queryBuilder.append(" != ");
         }
 
+        /**
+         * Find objects with values equal to the given value
+         * @param value the value to compare to this variable name
+         * @return a {@link CombinableFilterValue} that lets you add additional filters
+         */
         public CombinableFilterValue equal(String value) {
             equal();
             quote(value);
@@ -87,34 +127,65 @@ public class SearchQuery {
         }
 
         //Integer filter values
+        /**
+         * Find objects with values equal to the given value
+         * @param value the value to compare to this variable name
+         * @return a {@link CombinableFilterValue} that lets you add additional filters
+         */
         public CombinableFilterValue equal(int value) {
             equal();
             queryBuilder.append(value);
             return FilterValue();
         }
 
+        /**
+         * Find objects with values not equal to the given value
+         * @param value the value to compare to this variable name
+         * @return a {@link CombinableFilterValue} that lets you add additional filters
+         */
         public CombinableFilterValue notEqual(int value) {
             notEqual();
             queryBuilder.append(value);
             return FilterValue();
         }
 
+        /**
+         * Find objects with values less than the given value
+         * @param value the value to compare to this variable name
+         * @return a {@link CombinableFilterValue} that lets you add additional filters
+         */
         public CombinableFilterValue lessThan(int value) {
             queryBuilder.append(" < ");
             queryBuilder.append(value);
             return FilterValue();
         }
 
+        /**
+         * Find objects with values less than or equal to to the given value
+         * @param value the value to compare to this variable name
+         * @return a {@link CombinableFilterValue} that lets you add additional filters
+         */
         public CombinableFilterValue lessThanOrEqual(int value) {
             queryBuilder.append(" <= ").append(value);
             return FilterValue();
         }
-        
+
+        /**
+         * Find objects with values greater than the given value
+         * @param value the value to compare to this variable name
+         * @return a {@link CombinableFilterValue} that lets you add additional filters
+         */
         public CombinableFilterValue greaterThan(int value) {
             queryBuilder.append(" > ").append(value);
             return FilterValue();
         }
 
+
+        /**
+         * Find objects with values greater than or equal to the given value
+         * @param value the value to compare to this variable name
+         * @return a {@link CombinableFilterValue} that lets you add additional filters
+         */
         public CombinableFilterValue greaterThanOrEqual(int value) {
             queryBuilder.append(" >= ").append(value);
             return FilterValue();
@@ -126,17 +197,44 @@ public class SearchQuery {
 
     }
 
+    /**
+     * A completely specified filter, than can be further specified on a subObject or turned into a searchQuery string
+     */
     public interface FilterValue {
+
+        /**
+         * Drill down further
+         * @param name the variable name of the subobject to drill down to
+         * @return another SubObject
+         */
         public SubObject subObject(String name);
+
+        /**
+         * Return a valid search query for sending to CloudMine
+         * @return
+         */
         public String searchQuery();
     }
 
+    /**
+     * A {@link FilterValue} specific to geo queries
+     */
     public class CMGeoPointFilterValue implements FilterValue {
 
+        /**
+         * Only return objects within the specified distance
+         * @param distance
+         * @return
+         */
         public FilterValue within(Distance distance) {
             return within(distance.getMeasurement(), distance.getUnits());
         }
 
+        /**
+         * Only return objects within the specified distance
+         * @param distance
+         * @return
+         */
         public FilterValue within(double distance, DistanceUnits units) {
             queryBuilder.append(", ").append(distance).append(units.name());
             return this;
@@ -158,10 +256,35 @@ public class SearchQuery {
         }
     }
 
+    /**
+     * A filter that can be combined with other filters. Note that only one of AND or OR may be used in a combined filter,
+     * although the other methods are still available
+     */
     public interface CombinableFilterValue extends FilterValue{
+        /**
+         * Add another filter on a Property with the specified name
+         * @param name the variable name to filter on
+         * @return
+         */
         public PropertyName and(String name);
+
+        /**
+         * Add another filter on the same property that was just filtered on
+         * @return
+         */
         public PropertyName and();
+
+        /**
+         * Add a filter to be or'ed with the proceeding filters with the given name
+         * @param name the variable name to filter on
+         * @return
+         */
         public PropertyName or(String name);
+
+        /**
+         * Add a filter to be or'ed with the proceeding filters with the same name as the last filter
+         * @return
+         */
         public PropertyName or();
     }
 
